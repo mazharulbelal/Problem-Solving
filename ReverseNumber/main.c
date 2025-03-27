ec3c6b424657874f535d13480dcf4cc7ad684a2b
Ayat <ayat@MacBooks-MacBook-Pro.local>
2025-03-27 07:55:51 +0600

    Reverse number

---
+++ ReverseNumber/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ReverseNumber
+//
+//  Created by Ayat on 27/3/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
