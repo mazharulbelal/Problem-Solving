4c6e40e08c458363d587d09327dea1307fc711c0
Ayat <ayat@MacBooks-MacBook-Pro.local>
2025-03-31 16:34:16 +0600

    int to roman c solution add using C switch case

--- Integer To Roman/main.c
+++ Integer To Roman/main.c
@@ -8,6 +8,26 @@
 #include <stdio.h>
 #include "math.h"
 
+const char* getRoman(int num) {
+    switch (num) {
+        case 1000: return "M";
+        case 900: return "CM";
+        case 500: return "D";
+        case 400: return "CD";
+        case 100: return "C";
+        case 90: return "XC";
+        case 50: return "L";
+        case 40: return "XL";
+        case 10: return "X";
+        case 9: return "IX";
+        case 5: return "V";
+        case 4: return "IV";
+        case 1: return "I";
+        default: return "Invalid input";
+    }
+}
+
+
 int main(int argc, const char * argv[]) {
     unsigned short int number ;
     printf("Number: ");
@@ -29,8 +49,10 @@ int main(int argc, const char * argv[]) {
     }
     
     
+   
     for (int i = 0; i< digitCount; i++) {
-        printf("\n%d", numberList[i]);
+        printf("%s", getRoman(numberList[i]));
+
     }
     
    
