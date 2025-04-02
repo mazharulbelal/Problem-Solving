0e5ecdc2af03f4255ded6f1c2d7e154b8f0d04c2
Mazharul <mazharul.belal@gmail.com>
2025-04-02 23:28:56 +0600

    Way Too Long Words

---
+++ Assiut University Training - Newcomers/A. Way Too Long Words.cpp
@@ -0,0 +1,40 @@
+//
+//  A. Way Too Long Words.cpp
+//  Problem-solving
+//
+//  Created by Mazharul on 2/4/25.
+//
+
+
+
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    char inputText[n][101];
+    
+    for (int i = 0; i<n; i++) {
+        scanf("%s", inputText[i]);
+    }
+    
+    for (int i = 0; i< n; i++) {
+        int count = 0;
+        while (inputText[i][count] != '\0') {
+            count ++;
+            }
+        
+        if (count > 10) {
+            printf("%c%d%c\n", inputText[i][0], count - 2, inputText[i][count - 1]);
+        }
+        else {
+            printf("%s\n", inputText[i]);
+            }
+        }
+    }
+
+    
+
+
